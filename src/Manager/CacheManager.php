76d69066e5048dfb570796091a0ff73edb8ada2b
Axel Venet <kern046@gmail.com>
2016-03-01 18:19:03 +0100

    Implement CacheManager to handle cache for each changelog manager method

---
+++ src/Manager/CacheManager.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ChangelogParser\Manager;
+
+class CacheManager {
+    /** @var int **/
+    protected $cacheTime = 3600;
+    
+    /**
+     * @param string $file
+     * @param string $content
+     */
+    public function generateCache($file, $content) {
+        file_put_contents($this->generateCachePath($file), $content);
+    }
+    
+    /**
+     * @param string $file
+     * @return mixed
+     */
+    public function getCache($file) {
+        $path = $this->generateCachePath($file);
+        if(!is_file($path) || ($generationTime = filemtime($path)) === false || $generationTime < (time() - $this->cacheTime)) {
+            clearstatcache(true, $path);
+            return false;
+        }
+        $content = file_get_contents($path);
+        clearstatcache(true, $path);
+        return $content;
+    }
+    
+    /**
+     * @param string $file
+     * @return string
+     */
+    public function generateCachePath($file) {
+        return realpath(__DIR__ . "/../../data/cache") . "/$file.cache";
+    }
+}
